86942b7f24239eded7f8f144127b841023e87181
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/TreeView-example/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿Namespace TreeView_example
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.treeList1 = New DevExpress.XtraTreeList.TreeList()
+            Me.treeListColumn1 = New DevExpress.XtraTreeList.Columns.TreeListColumn()
+            CType(Me.treeList1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' treeList1
+            ' 
+            Me.treeList1.Columns.AddRange(New DevExpress.XtraTreeList.Columns.TreeListColumn() { Me.treeListColumn1})
+            Me.treeList1.Cursor = System.Windows.Forms.Cursors.Default
+            Me.treeList1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.treeList1.Location = New System.Drawing.Point(0, 0)
+            Me.treeList1.Name = "treeList1"
+            Me.treeList1.BeginUnboundLoad()
+            Me.treeList1.AppendNode(New Object() { "Node 1"}, -1)
+            Me.treeList1.AppendNode(New Object() { "Node 11"}, 0)
+            Me.treeList1.AppendNode(New Object() { "Node 2"}, -1)
+            Me.treeList1.AppendNode(New Object() { "Node 21"}, 2)
+            Me.treeList1.AppendNode(New Object() { "Node 22"}, 2)
+            Me.treeList1.AppendNode(New Object() { "Node 3"}, -1)
+            Me.treeList1.AppendNode(New Object() { "Node 31"}, 5)
+            Me.treeList1.EndUnboundLoad()
+            Me.treeList1.OptionsView.AutoWidth = False
+            Me.treeList1.Size = New System.Drawing.Size(348, 240)
+            Me.treeList1.TabIndex = 0
+            ' 
+            ' treeListColumn1
+            ' 
+            Me.treeListColumn1.Caption = "treeListColumn1"
+            Me.treeListColumn1.FieldName = "treeListColumn1"
+            Me.treeListColumn1.Name = "treeListColumn1"
+            Me.treeListColumn1.Visible = True
+            Me.treeListColumn1.VisibleIndex = 0
+            Me.treeListColumn1.Width = 122
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(348, 240)
+            Me.Controls.Add(Me.treeList1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(Me.treeList1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private treeList1 As DevExpress.XtraTreeList.TreeList
+        Private treeListColumn1 As DevExpress.XtraTreeList.Columns.TreeListColumn
+    End Class
+End Namespace
+
---
+++ VB/TreeView-example/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+
+Namespace TreeView_example
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            treeList1.ForceInitialize()
+
+            treeList1.Appearance.FocusedCell.BackColor = System.Drawing.SystemColors.Highlight
+            treeList1.Appearance.FocusedCell.ForeColor = System.Drawing.SystemColors.HighlightText
+            treeList1.OptionsBehavior.Editable = False
+
+            treeList1.ViewStyle = DevExpress.XtraTreeList.TreeListViewStyle.TreeView
+        End Sub
+    End Class
+End Namespace
